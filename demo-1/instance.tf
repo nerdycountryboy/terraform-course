57149328d438b9d2a9f84c0b28b22ce700af036c
nerdycountryboy <nerdycountryboy@protonmial.com>
2020-03-23 02:36:32 -0700

    Done for Today

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "example2" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
 }
--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -17,5 +17,3 @@ variable "AMIS" {
   }
 }
 
-  
-  
